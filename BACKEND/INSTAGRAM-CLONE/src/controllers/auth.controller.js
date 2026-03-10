4194006b8b521a76e0e4f9d42710133af4744fe5
Deepak-Codes2205 <deepak.contact122@gmail.com>
2026-03-11 02:16:50 +0530

    1ST DAY - REGISTER AND LOGIN

---
+++ BACKEND/INSTAGRAM-CLONE/src/controllers/auth.controller.js
@@ -0,0 +1,106 @@
+const express = require("express")
+const crypto = require("crypto")
+const authRouter = express.Router()
+const jwt = require("jsonwebtoken")
+
+async function loginController(req,res){
+    const{username, email, password, bio, profileImage} = req.body
+
+    //CHECKING WHETHER THE EMAIL OR USERNAME DOES EXISTS ALREADY
+    const isUserAlreadyExists = await userModel.findOne({
+        $or:[
+            { username },
+            { email }
+        ]
+    })
+    
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message:"User already exists" + (isUserAlreadyExists.email == email) ? "Email Already Exists" : "Username Already Exists"
+        })
+    }
+    //HASHING PASSWORD
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash,
+        bio,
+        profileImage
+    })
+
+
+    /* User ka data hona chahiye
+        or data unique hona chahiye */
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message: "User Registered Successfully",
+        user:{
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profileImage: user.profileImage
+            
+        }
+    })
+
+}
+
+async function registerController(req,res){
+    const {username,email,password} = req.body
+
+    /* CAN LOGIN ON THE BASIS OF EITHER
+    USERNAME AND PASSWORD 
+    OR EMAIL AND PASSWORD */
+
+    const user = await userModel.findOne({
+        $or:[
+            //CONDITIONS
+            {
+                username: username /* a */
+            },
+            {
+                email: email /* undefined */ //or vice versa
+            }
+        ]
+    })
+    if(!user){
+        return res.status(401).json({
+            message: "User Not Found"
+        })
+    }
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+     
+    const isValidPassword = hash === user.password
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message: "Incorrect Password"
+        })
+    }
+    const token = jwt.sign(
+        { id: user._id},
+        process.env.JWT_SECRET,{expiresIn:"1d"}
+    )
+    res.cookie("token",token)
+    res.status(201).json({
+        message: "User Loggined Successfully",
+        user: {
+            username : user.username,
+            email: user.email,
+            bio: user.bio,
+            profileImage: user.profileImage
+        }
+    })
+}
+
+module.exports = {
+    registerController,
+    loginController
+}

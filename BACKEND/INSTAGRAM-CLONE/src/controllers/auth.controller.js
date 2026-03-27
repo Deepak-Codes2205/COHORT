ee297b01b6193f55e490aed11e8fcb5767153ea6
Deepak-Codes2205 <deepak.contact122@gmail.com>
2026-03-27 07:28:47 +0530

    Passed username along with user id in token

--- BACKEND/INSTAGRAM-CLONE/src/controllers/auth.controller.js
+++ BACKEND/INSTAGRAM-CLONE/src/controllers/auth.controller.js
@@ -35,7 +35,8 @@ async function  registerController(req,res){
     /* User ka data hona chahiye
         or data unique hona chahiye */
     const token = jwt.sign({
-        id: user._id
+        id: user._id,
+        username: user.username
     }, process.env.JWT_SECRET,{expiresIn:"1d"})
 
     res.cookie("token",token)
@@ -86,7 +87,8 @@ async function loginController(req,res){
     }
     const token = jwt.sign(
         { 
-            id: user._id
+            id: user._id,
+            username: user.username
         },process.env.JWT_SECRET,{expiresIn:"1d"}
     )
 

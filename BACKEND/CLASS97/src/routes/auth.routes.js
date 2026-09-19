1f9b1abcfb66bddd387685ac8b9ec14eac46d86a
Deepak-Codes2205 <deepak.contact122@gmail.com>
2026-09-19 15:39:18 +0530

    style: improve code formatting and add missing documentation comments

--- BACKEND/CLASS97/src/routes/auth.routes.js
+++ BACKEND/CLASS97/src/routes/auth.routes.js
@@ -4,6 +4,10 @@ const jwt = require('jsonwebtoken')
 const authRouter = express.Router();
 const crypto = require("crypto")
 
+
+/**
+ * /api/auth/register 
+ */
 authRouter.post('/register', async(req,res)=>{
     const{name,email,password} = req.body
 
@@ -30,10 +34,8 @@ authRouter.post('/register', async(req,res)=>{
         },
         process.env.JWT_SECRET
     )
-
     //SETTING/PUTTING THE TOKENS WITHIN THE COOKIES (CLIENT'S SITE)
-    res.cookie("jwt_token",token)
-
+    res.cookie("jwt_token", token)
     res.status(201).json({
         message:"User registerd successfully",
         user,
@@ -42,7 +44,10 @@ authRouter.post('/register', async(req,res)=>{
 
 })
 
-// /api/auth/protected 
+
+/**
+ * /api/auth/protected 
+ */
 authRouter.post("/protected", (req,res)=>{
     console.log(req.cookies)
 
@@ -52,15 +57,19 @@ authRouter.post("/protected", (req,res)=>{
 })
 
 
-/* LOGIN-API : /api/auth/login */
+/**
+ * LOGIN-API : /api/auth/login 
+ */
 authRouter.post("/login", async(req,res)=>{
-    const {email,password} = req.body
+    const { email, password } = req.body
+
     const user = await userModel.findOne({email})
     if(!user){
         return res.status(404).json({
             message:"User not found with this email address"
         })
     }
+
     const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex") 
     if(!isPasswordMatched){
         return res.status(404).json({
@@ -74,7 +83,7 @@ authRouter.post("/login", async(req,res)=>{
         }
         ,process.env.JWT_SECRET)
 
-    res.cookie("jwt_token",token)
+    res.cookie("jwt_token", token)
     res.status(200).json({
         message:"User logged in",
         user
